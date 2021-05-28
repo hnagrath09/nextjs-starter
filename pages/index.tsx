a6b7f8cb7a2af2fc34ea8582c98f0a77d8656db9
Himanshu Nagrath <him.nagrath@gmail.com>
2021-05-28 10:44:44 +0530

    completed boiler plate setup with tailwind, eslint, prettier

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      Hello World!!!
-    </div>
-  );
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      Hello World!!!
+    </div>
+  )
+}
